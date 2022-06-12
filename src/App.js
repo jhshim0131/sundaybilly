7a3dafe9dd5661895d09e6ac5f5317a1111e39e2
RyuJihee <ryujihee01@gmail.com>
2022-06-12 15:22:52 +0900

    layout0612

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import React,{useState} from "react";
+import './css/app.css'
+import Navigation from './layout/Navigation'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Posting from './page/Posting'
+import Layout from "./layout/Layout";
 
 function App() {
   return (
     <div className="App">
-     <h1>sunday billy</h1>
-    </div>
+    {/* <Layout /> */}
+    <Navigation/>
+  </div>
   );
 }
 
